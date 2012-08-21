2b021312a82c7fe6aae8228759470a18847098c0
Simon Maddox <simon@simonmaddox.com>
2012-08-21 15:06:40 +0100

    Pressing Escape now dismisses select playlist panel

--- Le Code/LCSelectPlaylistPanel.m
+++ Le Code/LCSelectPlaylistPanel.m
@@ -9,7 +9,7 @@
 #import "LCSelectPlaylistPanel.h"
 #import "LCConstants.h"
 
-@interface LCSelectPlaylistPanel ()
+@interface LCSelectPlaylistPanel () <NSTextFieldDelegate>
 @property (strong, nonatomic) IBOutlet NSTextField *playlistField;
 @end
 
@@ -22,9 +22,24 @@
 
 - (IBAction)savePlaylist:(id)sender {
 	[[NSUserDefaults standardUserDefaults] setObject:self.playlistField.stringValue forKey:kPlaylistUserDefaultsKey];
-	[[NSApplication sharedApplication] endSheet:self.window];
-	
 	[[NSNotificationCenter defaultCenter] postNotificationName:kPlaylistChangedNotification object:nil];
+	[self hidePanel];
+}
+
+- (void) hidePanel {
+	[[NSApplication sharedApplication] endSheet:self.window];
+}
+
+- (BOOL)control:(NSControl *)control textView:(NSTextView *)textView doCommandBySelector:(SEL)commandSelector
+{
+	if ([NSStringFromSelector(commandSelector) isEqualToString:@"cancel:"]){
+		[self hidePanel];
+		return YES;
+	} else if ([NSStringFromSelector(commandSelector) isEqualToString:@"insertNewline:"]){
+		[self savePlaylist:control];
+		return YES;
+	}
+	return NO;
 }
 
 @end
